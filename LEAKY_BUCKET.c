21178c074dbd3552a55752409e644417c3d796ad
Antony Thomas <134713648+torcodex@users.noreply.github.com>
2025-04-05 00:30:11 +0530

    Create LEAKY_BUCKET.c

---
+++ LEAKY_BUCKET.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int no_of_queries, storage, output_pkt_size;
+int input_pkt_size, bucket_size, size_left;
+storage = 0;
+no_of_queries = 4;
+bucket_size = 10;
+input_pkt_size = 4;
+output_pkt_size = 1;
+for (int i = 0; i < no_of_queries; i++) {
+size_left = bucket_size - storage;
+if (input_pkt_size <= size_left) {
+storage += input_pkt_size;
+printf("Buffer size= %d out of bucket size= %d\n", storage, bucket_size);
+}
+else {
+printf("packet loss= %d\n", input_pkt_size - size_left );
+storage = bucket_size;
+printf("Buffer size= %d out of bucket size= %d\n", storage, bucket_size);
+}
+storage -= output_pkt_size;
+}
+return 0;
+}
